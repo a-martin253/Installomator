29481bff715c01c470850641ed51c947166bd719
Adam Martin <adam.martin@smartsheet.com>
2026-02-11 17:02:06 -0800

    Update Tableau Desktop to include download logic for architectures and update downloadURL pathes
    Tableau Desktop has changed their downloadURL pathes for Intel and Silicon versions and the current label auto redirects to the Intel version.
    
    This update will validate which chipset the Mac that is installing the app on has and then point to the proper downloadURL path for that architecture.

--- fragments/labels/tableaudesktop.sh
+++ fragments/labels/tableaudesktop.sh
@@ -2,7 +2,15 @@ tableaudesktop)
     name="Tableau Desktop"
     type="pkgInDmg"
     packageID="com.tableausoftware.tableaudesktop"
-    downloadURL="https://www.tableau.com/downloads/desktop/mac"
-    appNewVersion=${$(curl -fsIL "$downloadURL" | sed -nE 's/.*Desktop-([0-9-]*).*/\1/p')//-/.}
+    latestVersionURL="https://www.tableau.com/downloads/desktop/reg-mac-arm64"
+    versionString=$(curl -fsIL "${latestVersionURL}" | grep -i "location" | tail -1 | sed -nE 's/.*\/TableauDesktop-([0-9.-]+)\.dmg/\1/p')
+    appNewVersion=${versionString//-/.}
+    if [[ $(arch) == "arm64" ]]; then
+        log "Architecture is Apple Silicon (arm64), using arm64-specific download."
+        downloadURL="https://www.tableau.com/downloads/desktop/reg-mac-arm64"
+    else
+        log "Architecture is Intel (x86_64), using Universal download."
+        downloadURL="https://www.tableau.com/downloads/desktop/reg-mac"
+    fi
     expectedTeamID="QJ4XPRK37C"
     ;;
